08e05e30f8079c34393c56454fa543740fc38baa
ibrahimali-ca <ibrahin@ibrahimali.ca>
2021-11-24 13:42:45 -0500

    testing boilerplate deployment

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+// Footer.js
+const Footer = () => {
+    return (
+      <footer>
+        <h1>Created at Juno College by Ibrahim Ali</h1>
+      </footer>
+    )
+  }
+  
+  export default Footer;
+  
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+// Header.js
+const Header = () => {
+    return (
+      <header>
+        <h1>Sports Trivia</h1>
+      </header>
+    )
+  }
+  
+  export default Header;
+  
